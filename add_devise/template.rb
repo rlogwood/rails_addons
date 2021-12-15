a48fad7806b1c27fe1f7f1bfaf38f4c8e64a003a
Richard Logwood <richard.logwood.com>
2021-12-15 18:01:53 -0500

    update devise version and views to work rails 7

--- add_devise/template.rb
+++ add_devise/template.rb
@@ -10,8 +10,6 @@ require_relative '../lib/addon_helpers'
 # Apply the template
 # bin/rails app:template LOCATION=$template --trace
 
-# bin/rails app:template LOCATION=~/src/repos/public/rails_addons/add_devise/template.rb --trace
-
 ## ====================================
 ## Boilerplate for all templates: START
 ## bootstrap template utilities from
@@ -52,12 +50,36 @@ end
 ## Boilerplate for all templates: END
 ## ====================================
 
+# **
+# ** Template starts here
+# **
+
+DEVISE_ROUTES = <<-ROUTES.freeze
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+ROUTES
+
+USE_TURBO_FOR_DEVISE = <<-SOURCE.freeze
+  # devise views don't currently work with turbo
+  def turbo_for_devise?
+    false
+  end
+SOURCE
+
+ES_BUILD_COMMANDS = <<-SOURCE.freeze
+    "show_esbuild" : "bin/rails esbuild:show",
+    "kill_esbuild" : "bin/rails esbuild:kill",
+SOURCE
 
 # Create a standard devise install with everything enabled
 # Add additional attributes username:string and role:string
 # NOTE: initial source https://railsbytes.com/public/templates/X8Bsjx
 def add_devise
+  # see https://stackoverflow.com/a/69748177/960184
   gem 'devise', git: 'https://github.com/heartcombo/devise', branch: 'main'
+
+  # waiting for fix on devise release, use main branch in the meantime
   # gem 'devise'
 
   #run "bundle add devise"
@@ -69,22 +91,30 @@ def add_devise
   model_name = ask("What do you want to call your Devise model [user] ?")
   model_name = "user" if model_name.blank?
   attributes = ""
-  if yes?("Do you want to any extra attributes to #{model_name}? [y/n]")
+  if yes?("Do you want to any extra attributes to #{model_name}? [y/N]")
     attributes = ask("What attributes?")
   end
 
-  # NOTE: on railsbytes there is this note about installing devise
+  # TODO: Note, check why on railsbytes there is this note about installing devise
   # We don't use rails_command here to avoid accidentally having RAILS_ENV=development as an attribute.
   # not sure I understand why this is done this way, got this from studying a railsbytes comment
   # run "rails generate devise #{model_name} #{attributes}"
 
   generate "devise", model_name, attributes
-  #generate "devise:views"
+
+  # not using standard default devise views created by generate "devise:views"
+  # instead use tailwind modified devise views
+  copy_devise_views
 
   update_devise_db_migration
 
-  # user tailwind modified devise views
-  copy_devise_views
+  insert_into_file('config/routes.rb', after: "devise_for :users\n") do
+    DEVISE_ROUTES
+  end
+
+  insert_into_file('app/helpers/application_helper.rb', after: "module ApplicationHelper\n") do
+    USE_TURBO_FOR_DEVISE
+  end
 end
 
 # TODO: research this, from Chris' RailsBytes for devise
@@ -106,6 +136,7 @@ def copy_devise_views
   directory AddonHelpers.source_dir(devise_views_dir), devise_views_dir
 end
 
+# load the utility functions needed for automatically downloading template from repo if needed
 repo_require('https://raw.githubusercontent.com/rlogwood/rails_addons/main',
              'lib/config_paths.rb', '..')
 
@@ -113,12 +144,3 @@ repo_path = 'https://github.com/rlogwood/rails_addons.git'
 template_dir = add_template_repository_to_source_path(__FILE__, repo_path)
 puts "Template directory is #{template_dir}"
 add_devise
-
-
-# if yes?("Would you like to install Devise?")
-#   gem "devise"
-#   generate "devise:install"
-#   model_name = ask("What would you like the user model to be called? [user]")
-#   model_name = "user" if model_name.blank?
-#   generate "devise", model_name
-# end
