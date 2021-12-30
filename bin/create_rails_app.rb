0349095e7e7b28ee6a56f55157c9a8b9f98b69b1
Richard Logwood <richard.logwood.com>
2021-12-30 00:50:25 -0500

    cleanup creation script

--- bin/create_rails_app.rb
+++ bin/create_rails_app.rb
@@ -1,47 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-## ====================================
-## Boilerplate for all templates: START
-## bootstrap template utilities from
-## repo if not found locally
-## ------------------------------------
-# try local load
-def local_require(filename, relative_path)
-  relative_flname = File.join(relative_path, filename)
-  require_relative(relative_flname)
-end
-
-# try loading locally first, try repo version on load error
-# caution: only use with files you control access to!
-def repo_require(raw_repo_prefix, filename, relative_path = '')
-  local_require(filename, relative_path)
-
-rescue LoadError => e
-  puts e.message
-  require 'open-uri'
-
-  tempdir = Dir.mktmpdir("repo_require-")
-  temp_flname = File.join(tempdir, File.basename(filename))
-  return false if $LOADED_FEATURES.include?(temp_flname)
-
-  remote_flname = File.join(raw_repo_prefix, filename)
-  puts "file not found locally, checking repo: #{remote_flname}"
-  begin
-    File.open(temp_flname, 'w') do |f|
-      f.write(URI.parse(remote_flname).read)
-    end
-  rescue OpenURI::HTTPError => e
-    raise "Error: Can't load #{filename} from repo: #{e.message} - #{remote_flname}"
-  end
-  require(temp_flname)
-  FileUtils.remove_entry(tempdir)
-end
-## ------------------------------------
-## Boilerplate for all templates: END
-## ====================================
-
-
 def sys(cmd, *args, **kwargs)
   puts("\n*** running: \e[1m\e[33m#{cmd} #{args}\e[0m\e[22m\n\n")
   system(cmd, *args, exception: true, **kwargs)
@@ -54,7 +13,9 @@ def create_rails_7_app_with_tailwind(app_name)
   sys("rails new #{app_name} -m #{template_file} -j esbuild --css tailwind --database postgresql")
 end
 
-def apply_templates(app_name, location)
+def apply_templates(app_name)
+
+  location = File.join(File.dirname(__FILE__),"..")
 
   templates = %w[rails7_tailwind_config
                  add_tailwind_scaffold
@@ -82,19 +43,16 @@ def apply_templates(app_name, location)
 end
 
 def create_admin_user
-  create_admin_user_script = 'create_admin_user.rb'
-  sys('bin/rails runner #{create_admin_user_script}')
+  create_admin_user_script = File.join(File.dirname(__FILE__), 'create_admin_user.rb')
+  sys("bin/rails runner #{create_admin_user_script}")
 end
 
 print "Enter your app name: "
 app_name = gets.chomp
+create_rails_7_app_with_tailwind(app_name)
 
-# create_rails_7_app_with_tailwind(app_name)
-
-location='~/src/repos/public/rails_addons'
 Dir.chdir(File.join(Dir.pwd,app_name))
-
-# apply_templates(app_name)
-
+apply_templates(app_name)
 create_admin_user
+
 
